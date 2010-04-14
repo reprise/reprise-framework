ddbae28673f253b0bc20ca34b4a1369853baac7d
Till Schneidereit <tschneidereit@gmail.com>
2010-04-14 17:09:09 +0200

    updated Trazzle to version 1.5.1.1

--- rakefile.rb
+++ rakefile.rb
@@ -11,7 +11,7 @@ sprout 'as3'
 
 project = project_model :model do |m|
   m.project_name            = 'Reprise'
-  m.version                 = '0.5'
+  m.version                 = '0.5.1'
   m.language                = 'as3'
   m.doc_dir                  = "doc"
   m.doc_out                  = "#{m.doc_dir}/bin/asdoc"
